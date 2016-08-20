47ed278e25b722af1f6e4007d213b7d63539e774
unknown <aki_F@aki.sjtu.edu.cn>
2016-08-20 18:23:29 +0800

    1. Optimized the program 2. Add two new functions for On/Off plotting

--- Kinematic_Analysis.m
+++ Kinematic_Analysis.m
@@ -4,18 +4,18 @@ clc;
 close all;
 %%
 [NAME,SUBN,TYPE,TIME,SIDE,TASK] = initialization;
-% TYPE = {'NMES'; 'SHAM'};
-% TIME = {'AFT'; 'PRE'; 'IME'};
+% TYPE = {'SHAM'; 'NMES'};
+% TIME = {'PRE'; 'AFT'; 'IME'};
 % SIDE = {'L'; 'R'};
-% TASK = {'LR'; 'FR'};
+% TASK = {'FR'; 'LR'};
 
 root = 'D://FES2015//Kinematics_Analysis';
-for SN_name = 14
+for SN_name = 14 % 6,11,14
     for SN_subn = SN_name
-        for SN_type = 2
-            for SN_time = [1,3]
+        for SN_type = 1:2
+            for SN_time = 3
                 for SN_side = 2
-                    for SN_task = 2
+                    for SN_task = 1:2
                         name = NAME{SN_name};
                         subn = SUBN{SN_subn};
                         type = TYPE{SN_type};
@@ -25,7 +25,9 @@ for SN_name = 14
                         Dir = [root '//Kinematics_Data//' subn '_' type '_' time '_' side '_' task];
                         Axisfile = [root '//Kinematics_Data//Axis_num_' subn '_' task '.mat'];
 %                         KinematicAnalysis(root,Dir,Axisfile);
-                        PlotOnOffTogether(SN_type,subn,task,root,Dir,Axisfile);
+                        PlotOnOffTogether(SN_type,subn,task,root,Dir,Axisfile); % SHAM:BLUE; NMES:RED
+                        PlotOnOffSeparate(SN_type,subn,type,task,root,Dir,Axisfile); % SHAM:BLUE; NMES:RED
+%                         PlotOnOffShadow(SN_type,subn,task,root,Dir,Axisfile); % SHAM:BLUE; NMES:RED
                     end
                 end
             end
--- initialization.m
+++ initialization.m
@@ -1,8 +1,8 @@
 function [NAME,SUBN,TYPE,TIME,SIDE,TASK] = initialization
 NAME = {'无'; '无'; '无'; '无'; '刘海方'; '赵增起'; '王旭峰'; '李昌'; '毛建华'; '杨鸿伟'; '俞建国'; '李祖侃'; '郭新'; '徐建业'};
 SUBN = {'S01'; 'S02'; 'S03'; 'S04'; 'S05'; 'S06'; 'S07'; 'S08'; 'S09'; 'S10'; 'S11'; 'S12'; 'S13'; 'S14'};
-TYPE = {'NMES'; 'SHAM'};
-TIME = {'AFT'; 'PRE'; 'IME'};
+TYPE = {'SHAM'; 'NMES'};
+TIME = {'PRE'; 'AFT'; 'IME'};
 SIDE = {'L'; 'R'};
-TASK = {'LR'; 'FR'};
+TASK = {'FR'; 'LR'};
 end
